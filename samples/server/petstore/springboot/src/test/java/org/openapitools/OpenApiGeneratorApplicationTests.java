a4f52bc0fd032b458b4e614eed74e19e761bcb21
Bastien JANSEN <bastien.jansen@ca-ps.com>
2023-03-02 15:11:20 +0100

    reproduce issue

--- samples/server/petstore/springboot/src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
+++ samples/server/petstore/springboot/src/test/java/org/openapitools/OpenApiGeneratorApplicationTests.java
@@ -1,13 +1,30 @@
 package org.openapitools;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class OpenApiGeneratorApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException, InterruptedException {
+        var req = HttpRequest.newBuilder(URI.create("http://localhost:8066/v2/issue"))
+            .POST(HttpRequest.BodyPublishers.ofString("listOfStrings=hello"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .build();
+
+        var client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertThat(response.statusCode()).isEqualTo(200);
+
     }
 
 }
